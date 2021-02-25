312dbf0e3d2f73e638b61d110a41da23bbf2cdb1
Brian <brianbatz03@gmail.com>
2021-02-25 01:55:46 -0600

    Actualizacion de mapa y personajes

---
+++ assets/ResouceMaps/tileSnow_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileSnow_tile" tilewidth="65" tileheight="89" tilecount="1" columns="1">
+ <image source="../hexagontiles/Tiles/tileSnow_tile.png" width="65" height="89"/>
+</tileset>
